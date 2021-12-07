f9302787ec6b6e484cb4df876e51967616e7d93b
Alina <42976128+lesslyrics@users.noreply.github.com>
2021-12-07 17:42:28 +0300

    added proper input reading

--- multiagents/src/main/java/multiAgentAverage/average/SingleAgent.java
+++ multiagents/src/main/java/multiAgentAverage/average/SingleAgent.java
@@ -1,5 +1,8 @@
 package multiAgentAverage.average;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import jade.core.Agent;
 
@@ -8,6 +11,7 @@ public class SingleAgent extends Agent {
 
     private String[] agentsGraph;
     private float value;
+    private final String graphLocation = "src/main/java/multiAgentAverage/resources/graph.txt";
 
     @Override
     protected void setup() {
@@ -27,20 +31,34 @@ public class SingleAgent extends Agent {
         }
 
         // set agents' connections (graph edges)
-        String[][] graph = {
-                {"3","2"},
-                {"1","5"},
-                {"2","4"},
-                {"3","6"},
-                {"4","1"},
-                {"2","7"},
-                {"7","3"},
-                {"5","6"}
-        };
+        String[][] graph = new String[0][];
+        try {
+            graph = setAgentsGraph();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         agentsGraph = graph[id];
         addBehaviour(new FindAverageValue(this, TimeUnit.SECONDS.toMillis(1)));
     }
+
+    private String[][] setAgentsGraph() throws Exception {
+        Scanner sc = new Scanner(new BufferedReader(new FileReader(graphLocation)));
+        String[] size = sc.nextLine().trim().split(" ");
+
+        int rows = Integer.parseInt(size[0]);
+        int columns = Integer.parseInt(size[1]);
+
+        String[][] graph = new String[rows][columns];
+        while(sc.hasNextLine()) {
+            for (String[] strings : graph) {
+                String[] line = sc.nextLine().trim().split(" ");
+                System.arraycopy(line, 0, strings, 0, line.length);
+            }
+        }
+        return graph;
+    }
+
 
     public String[] getAgentsGraph() {
         return agentsGraph;
